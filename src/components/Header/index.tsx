cdf82522a9e6d586163e8a13e99accba5b1106ea
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-29 01:34:52 -0300

    finish all tests on header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header() {
   // TODO
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="spacetraveling" />
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
     </header>
   );
 }
